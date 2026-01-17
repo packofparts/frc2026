ef1d5fd919148acfac27cba13dbd94284ebf37b7
Seonmok <seonmok.jang@gmail.com>
2026-01-17 13:36:37 -0800

    log hub activation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,8 +5,10 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.utils.HubTracker;
 
 public class Robot extends TimedRobot {
     private Command m_autonomousCommand;
@@ -52,6 +54,7 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
+        SmartDashboard.putBoolean("Hub Active:", HubTracker.isAllianceHubActive());
     }
 
     @Override
