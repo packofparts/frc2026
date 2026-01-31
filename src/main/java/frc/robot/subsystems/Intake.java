fe311b0a16f03199b068eddf5ff574a9798096c7
Seonmok <seonmok.jang@gmail.com>
2026-01-31 12:54:50 -0800

    updated intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkMax;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -14,9 +13,8 @@ import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
     private static Intake instance;
-    private final SparkMax motor;
-    private final DigitalInput beamBreak;
-
+    private final SparkMax leadMotor;
+    private final SparkMax followerMotor;
     public static Intake getInstance() {
         if (instance == null) {
             instance = new Intake();
@@ -26,8 +24,8 @@ public class Intake extends SubsystemBase {
     }
 
     private Intake() {
-        motor = Constants.Intake.MOTOR_CONFIG.createSparkMax();
-        beamBreak = new DigitalInput(Constants.Intake.BEAMBRAKE_ID);
+        leadMotor = Constants.Intake.MOTOR_CONFIG.createSparkMax();
+        followerMotor = Constants.Intake.FOLLOWER_CONFIG.createSparkMax();
     }
 
     /**
@@ -35,7 +33,7 @@ public class Intake extends SubsystemBase {
      * @return the Command that runs the indexer
      */
     public Command runIntake() {
-        return run(() -> motor.set(Constants.Intake.SPEED));
+        return run(() -> leadMotor.set(Constants.Intake.SPEED));
     }
 
     /**
@@ -43,7 +41,7 @@ public class Intake extends SubsystemBase {
      * @return the Command that stops the indexer
      */
     public Command stopIntake() {
-        return run(() -> motor.set(0.0));
+        return run(() -> leadMotor.set(0.0));
     }
 
     /**
@@ -51,17 +49,12 @@ public class Intake extends SubsystemBase {
      * @return the Command that runs the indexer on reverse
      */
     public Command reverseIntake() {
-        return run(() -> motor.set(-Constants.Intake.SPEED));
-    }
-
-    public boolean beamBreakTriggered() {
-        return !beamBreak.get();
+        return run(() -> leadMotor.set(-Constants.Intake.SPEED));
     }
 
     @Override
     public void periodic() {
         super.periodic();
-        SmartDashboard.putNumber("Motor Speed", motor.get());
-        SmartDashboard.putBoolean("Intake Beam Break Triggered", beamBreakTriggered());
+        SmartDashboard.putNumber("Motor Speed", leadMotor.get());
     }
 }
